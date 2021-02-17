9c9362ab33f6b0d920c93937f8cf794c6ac7e6ad
giedrius200 <48772520+giedrius200@users.noreply.github.com>
2021-02-17 18:45:36 +0200

    Add files via upload

---
+++ lib.h
@@ -0,0 +1,26 @@
+#ifndef lib_H_INCLUDED
+#define lib_H_INCLUDED
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+
+const int cinTerminator = 0;
+struct Studentas {
+    string vardas, pavarde;
+    vector<double> paz;
+    double paz1[500];
+    double egz, gal, mediana;
+};
+
+double mediana(vector<double>);
+void isvedimas(Studentas *grupe, int);
+#endif 

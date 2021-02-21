032b997286c12bc8b7cdbf35a0c5a93b762d3af5
giedrius200 <48772520+giedrius200@users.noreply.github.com>
2021-02-21 17:14:37 +0200

    Add files via upload

--- lib.h
+++ lib.h
@@ -1,18 +1,26 @@
 #ifndef lib_H_INCLUDED
 #define lib_H_INCLUDED
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <fstream>
+
+#include <cstdlib>
+
+#include <sstream>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
+
+using std::ifstream;
 
 const int cinTerminator = 0;
 struct Studentas {
@@ -23,5 +31,5 @@ struct Studentas {
 };
 
 double mediana(vector<double>);
-void isvedimas(Studentas *grupe, int);
+void isvedimas(Studentas *grupe, int,char);
 #endif 

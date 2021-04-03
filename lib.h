7a225e8c050f520be6ec1959b2dd1ac133ba47d1
giedrius200 <48772520+giedrius200@users.noreply.github.com>
2021-04-03 11:16:27 +0300

    Add files via upload

--- lib.h
+++ lib.h
@@ -1,18 +1,42 @@
 #ifndef lib_H_INCLUDED
 #define lib_H_INCLUDED
 
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <iomanip>
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <fstream>
+
+#include <chrono>
+
+#include <cstdlib>
+
+#include <thread>
+
+#include <sstream>
+
+#include <list>
+
+#include <deque>
 
 using std::cin;
 using std::cout;
 using std::endl;
 using std::vector;
 using std::string;
+
+using std::ifstream;
+using namespace std;
+using namespace std::chrono;
+using namespace std::chrono_literals;
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration_cast;
+using std::chrono::duration;
+using std::chrono::milliseconds;
 
 const int cinTerminator = 0;
 struct Studentas {
@@ -23,5 +47,10 @@ struct Studentas {
 };
 
 double mediana(vector<double>);
-void isvedimas(Studentas *grupe, int);
+void isvedimas(Studentas *grupe, int,char);
+void sukurimas();
+void rusiavimas(string, int);
+void rusiavimasl(string, int);
+void rusiavimasr(string, int);
+
 #endif 
